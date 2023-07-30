f54f6601cdf890bda1be2ce003bb77db3b3b57ab
Siim Nelis <siim.nelis@gmail.com>
2023-07-30 11:14:32 +0300

    remove .net teavitushost

--- Xtee.Connector/Connected Services/TeavitusTeenus/TeavitusTeenus.cs
+++ Xtee.Connector/Connected Services/TeavitusTeenus/TeavitusTeenus.cs
@@ -798,7 +798,7 @@ namespace Xtee.Connector.TeavitusTeenus
             return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginOpen(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndOpen));
         }
         
-        public virtual System.Threading.Tasks.Task CloseAsync()
+        public new virtual System.Threading.Tasks.Task CloseAsync()
         {
             return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginClose(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndClose));
         }
